4578f847645f201a9153279a8ea9b57a3ca0bf2a
Hosam Sultan <hosam5553@gmail.com>
2021-07-27 21:45:55 +0200

    Base code set

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser= require('body-parser');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +14,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+const server = app.listen(port, ()=>{
+    console.log(`Server running on port ${port}. Check it out at http://localhost:${port}/`)
+});
+
+app.get("/", (request, response) => {
+    response.send("hey, hey Heyyyy, Got me!");
+  });
+  
+  app.post("/", (request, response) => {
+    data.push(request.body);
+  });
+  
--- website/app.js
+++ website/app.js
@@ -1,5 +1,16 @@
 /* Global Variables */
+const BASE_URL = 'api.openweathermap.org/data/2.5/weather?q=';
+const API_KEY ='&APPID=a223e187698cd909106500621d6bbd62';
+
+const zipCode = document.getElementById('zip');
+const feelingsArea = document.getElementById('feelings');
+const submitBtn = document.getElementById('generate');
+const dateArea = document.getElementById('date');
+const tempArea = document.getElementById('temp');
+const contentArea = document.getElementById('content');
+
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
