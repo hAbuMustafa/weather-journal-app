e21f6953a91361caef1db3bee84101613858d81c
Hosam Sultan <hosam5553@gmail.com>
2021-07-28 02:50:11 +0200

    fixed Month mistake to accomodate for zero-based fact

--- website/app.js
+++ website/app.js
@@ -12,7 +12,7 @@ const contentArea = document.getElementById("content");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
 
 submitBtn.addEventListener("click", buttonClicked);
 
